84a752f9871e744dc3c0fb9738c3151821877147
nikolai921 <nik.finnsailing@gmail.com>
2019-03-05 17:53:32 +0300

    Last task

--- function.php
+++ function.php
@@ -4,9 +4,19 @@
  *Обработка данных полученых из формы
  */
 
+function DataFromForm($link)
+{
+    $query = "SELECT short_name FROM type_ts_table";
+    $result = mysqli_query($link, $query) or die(mysqli_error($link));
+    for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
+    return $data;
+}
+
+
+
 function formDataSend($link)
 {
-
+//    print_r($_POST);
     if(!empty($_POST))
     {
     $dataForm = [
@@ -24,20 +34,19 @@ function formDataSend($link)
     $violations = mysqli_real_escape_string($link, $_POST['violations'])
     ];
 
-
     $select = [
-      "SELECT id FROM type_ts_table WHERE short_name = $type_ts",
-      "SELECT id FROM Engine_power_table WHERE short_name = $engine_power",
-      "SELECT id FROM City_registration_table WHERE short_name = $city_registration",
-      "SELECT id FROM User_options_table WHERE number_drivers_limited = $number_drivers",
-      "SELECT id FROM Insurance_period_table WHERE short_name = $insurance_period",
-      "SELECT id FROM KBM_table WHERE short_name = $KBM",
-      "SELECT id FROM Age_drivers_table WHERE short_name = $age_drivers",
-      "SELECT id FROM Experience_drivers_table WHERE short_name = $experience_drivers",
-      "SELECT id FROM User_options_table WHERE foreigner = $foreigner",
-      "SELECT id FROM User_options_table WHERE legal_form = $legal_form",
-      "SELECT id FROM Period_use_table WHERE short_name = $period_use",
-      "SELECT id FROM User_options_table WHERE violations = $violations",
+      "SELECT id FROM type_ts_table WHERE short_name = '$type_ts'",
+      "SELECT id FROM engine_power_table WHERE short_name = '$engine_power'",
+      "SELECT id FROM city_registration_table WHERE short_name = '$city_registration'",
+      "SELECT id FROM user_options_table WHERE number_drivers_limited = '$number_drivers'",
+      "SELECT id FROM insurance_period_table WHERE short_name = '$insurance_period'",
+      "SELECT id FROM KBM_table WHERE short_name = '$KBM'",
+      "SELECT id FROM age_drivers_table WHERE short_name = '$age_drivers'",
+      "SELECT id FROM experience_drivers_table WHERE short_name = '$experience_drivers'",
+      "SELECT id FROM user_options_table WHERE foreigner = '$foreigner'",
+      "SELECT id FROM user_options_table WHERE legal_form = '$legal_form'",
+      "SELECT id FROM period_use_table WHERE short_name = '$period_use'",
+      "SELECT id FROM user_options_table WHERE violations = '$violations'",
     ];
 
     foreach ($select as $elem)
@@ -46,21 +55,54 @@ function formDataSend($link)
         $index[] = mysqli_fetch_assoc($result);
     }
 
-    $query = "INSERT INTO main_table SET 
-    engine_power='$index[0]',
-    city_registration='$index[1]',
-    number_drivers='$index[2]',
-    insurance_period='$index[3]',
-    KBM='$index[4]',
-    KPt='$index[5]',
-    age_drivers='$index[6]',
-    experience_drivers='$index[7]',
-    foreigner='$index[8]',
-    legal_form='$index[9]',
-    period_use='$index[10]',
-    violations='$index[11]'";
+        $type_ts=$index[0]['id'];
+        $engine_power=$index[1]['id'];
+        $city_registration=$index[2]['id'];
+        $number_drivers=$index[3]['id'];
+        $insurance_period=$index[4]['id'];
+        $KBM=$index[5]['id'];
+        $age_drivers=$index[6]['id'];
+        $experience_drivers=$index[7]['id'];
+        $foreigner=$index[8]['id'];
+        $legal_form=$index[9]['id'];
+        $period_use=$index[10]['id'];
+        $violations=$index[11]['id'];
+
+        if($type_ts == 1 && $legal_form == 'Юр. лицо')
+        {
+            $KPt = 1;
+        } elseif ($type_ts == 4)
+        {
+            $KPt = 2;
+        } elseif ($type_ts == 5)
+        {
+            $KPt = 3;
+        } elseif ($type_ts == 7)
+        {
+            $KPt = 4;
+        } else
+        {
+            $KPt = 5;
+        }
+
+    $query = "INSERT INTO main_table SET
+    type_ts='$type_ts',
+    engine_power='$engine_power',
+    city_registration='$city_registration',
+    number_drivers='$number_drivers',
+    insurance_period='$insurance_period',
+    KBM='$KBM',
+    age_drivers='$age_drivers',
+    experience_drivers='$experience_drivers',
+    foreigner='$foreigner',
+    legal_form='$legal_form',
+    period_use='$period_use',
+    violations='$violations', 
+    KPt='$KPt'";
 
     $result = mysqli_query($link, $query) or die(mysqli_error($link));
+
 }
 }
+
 
--- treatment.php
+++ treatment.php
@@ -2,4 +2,21 @@
 
 include 'function.php';
 
-print_r(formDataSend());
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
+//Устанавливаем доступы к базе данных:
+$host = 'localhost'; //имя хоста, на локальном компьютере это localhost
+$user = 'root'; //имя пользователя, по умолчанию это root
+$password = '13579'; //пароль, по умолчанию пустой
+$db_name = 'osago.rarus'; //имя базы данных
+
+//Соединяемся с базой данных используя наши доступы:
+$link = mysqli_connect($host, $user, $password, $db_name);
+
+//Устанавливаем кодировку (не обязательно, но поможет избежать проблем):
+mysqli_query($link, "SET NAMES 'utf8'");
+
+print_r(DataFromForm($link));
+$name_type = ormDataSend($link);
+
